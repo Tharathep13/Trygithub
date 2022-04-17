706a488ee0d9dfd7d51ad2309b2cef0fdd06564a
Tharathep13 <devhunger16@gmail.com>
2022-04-17 23:15:29 +0700

    Initial commit

---
+++ src/calculator.java
@@ -0,0 +1,9 @@
+public class calculator {
+    public static void main (String[]arg){
+        int num1=1;
+        int num2= 2 ;
+        int result= num1+num2;
+        System.out.println("Thara first calculator");
+        System.out.println("num1 " + "+" + "num2 " +  "=" +result);
+    }
+}
