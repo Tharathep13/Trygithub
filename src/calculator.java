35c65c43df7b68329866ea0bc892063c56f5452b
Tharathep13 <devhunger16@gmail.com>
2022-04-17 23:33:01 +0700

    second calculator subtract only

--- src/calculator.java
+++ src/calculator.java
@@ -2,8 +2,8 @@ public class calculator {
     public static void main (String[]arg){
         int num1=1;
         int num2= 2 ;
-        int result= num1+num2;
-        System.out.println("Thara first calculator");
-        System.out.println("num1 " + "+" + "num2 " +  "=" +result);
+        int result= num1-num2;
+        System.out.println("Thara second calculator");
+        System.out.println("num1 " + "-" + "num2 " +  "=" +result);
     }
 }
