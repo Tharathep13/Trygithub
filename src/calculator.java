bda64e95e8de9a7bda3243eb579723f4d2febebf
Tharathep13 <devhunger16@gmail.com>
2022-04-17 23:53:32 +0700

    fourth calculator divide only

--- src/calculator.java
+++ src/calculator.java
@@ -2,8 +2,8 @@ public class calculator {
     public static void main (String[]arg){
         int num1=1;
         int num2= 2 ;
-        int result= num1*num2;
+        int result= num1/num2;
         System.out.println("Thara third calculator");
-        System.out.println("num1 " + "*" + "num2 " +  "=" +result);
+        System.out.println("num1 " + "/" + "num2 " +  "=" +result);
     }
 }
